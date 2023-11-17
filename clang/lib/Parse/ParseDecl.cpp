a23e506bf61542007a5975bf2f95988f689aab13
erichkeane <ekeane@nvidia.com>
2023-11-17 07:28:33 -0800

    [OpenACC] Rename ParseOpenACCDirective to ParseOpenACCDirectiveDecl
    The former name is more useful as a callee of the function in a future
    patch, so as suggested in that review, move the rename here.

--- clang/lib/Parse/ParseDecl.cpp
+++ clang/lib/Parse/ParseDecl.cpp
@@ -4742,7 +4742,7 @@ void Parser::ParseStructUnionBody(SourceLocation RecordLoc,
     }
 
     if (Tok.is(tok::annot_pragma_openacc)) {
-      ParseOpenACCDirective();
+      ParseOpenACCDirectiveDecl();
       continue;
     }
 
