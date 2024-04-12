5aed948216ce5061006b75f24a45f411b4c77e34
erichkeane <ekeane@nvidia.com>
2024-04-12 14:44:20 -0700

    [NFC] Clang-format previous OpenACC Patch

--- clang/lib/AST/StmtProfile.cpp
+++ clang/lib/AST/StmtProfile.cpp
@@ -2439,8 +2439,7 @@ public:
   }
 
 #define VISIT_CLAUSE(CLAUSE_NAME)                                              \
-  void Visit##CLAUSE_NAME##Clause(                                      \
-      const OpenACC##CLAUSE_NAME##Clause &Clause);
+  void Visit##CLAUSE_NAME##Clause(const OpenACC##CLAUSE_NAME##Clause &Clause);
 
 #include "clang/Basic/OpenACCClauses.def"
 };
@@ -2449,8 +2448,7 @@ public:
 void OpenACCClauseProfiler::VisitDefaultClause(
     const OpenACCDefaultClause &Clause) {}
 
-void OpenACCClauseProfiler::VisitIfClause(
-    const OpenACCIfClause &Clause) {
+void OpenACCClauseProfiler::VisitIfClause(const OpenACCIfClause &Clause) {
   assert(Clause.hasConditionExpr() &&
          "if clause requires a valid condition expr");
   Profiler.VisitStmt(Clause.getConditionExpr());
